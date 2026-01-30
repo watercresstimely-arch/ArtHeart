b7886103618cde87010a0748279ca4a4c784ec5b
watercresstimely-arch <watercresstimely@gmail.com>
2026-01-30 21:48:58 +0200

    Raise error if DISCORD_TOKEN is not set
    Add error handling for missing DISCORD_TOKEN environment variable

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import os
 # ========== CONFIG ==========
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+if not TOKEN:
+    raise RuntimeError("DISCORD_TOKEN environment variable not set")
+
+
 TARGET_CHANNEL_ID = 1466754024131919937 
 WAIT_SECONDS = 10800  # 3 hours sjsjsnsis
 REACTION_EMOJI = "❤️"
